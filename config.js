5bb313e3608a7f526076ba935649e5256ab9911f
uwuyenn <87053232+uwuyenn@users.noreply.github.com>
2021-07-07 10:35:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Phương vy à!',
+    introDesc: `Đây là quà của
+    Thu Uyn nha, nên hơi khác bọt tí`,
+    btnIntro: 'Ấn vào đây để nhận qà',
+    title: 'Thật ga là cũng hong biết tặng gì',
+    desc: 'Ủa shao bảo hong thít qà mà vẫn tìm tới đây z, hẳn là iu tui lúm chứ gì ',
+    btnYes: 'Ùm quá chuẩn',
+    btnNo: 'Hong đâu, mơ i',
+    question:'Ỏ tại shao vẫn tương tư người iu cũ vậy bbi',
+    btnReply: 'gửi',
+    reply: 'tại uyn dang iu qua ma :(',
+    mess: 'ha, tui biết mà',
+    messDesc: 'hết òi, nói chung là sinh nhat vuiiii',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
